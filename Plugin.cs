cdebbec53eb8e3c1635f099a886517cc1f5f9350
ShinyGorilla <134111158+ShinyGorilla@users.noreply.github.com>
2023-12-17 14:49:10 -0700

    A lot of crap added

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using BepInEx;
 using GorillaLocomotion;
 using GorillaNetworking;
@@ -11,6 +11,7 @@ using ComputerPlusPlus;
 using Valve.VR.InteractionSystem;
 using Oculus.Platform;
 using UnityEngine.XR.Interaction.Toolkit.AffordanceSystem.Receiver.Primitives;
+using GorillaLocomotion.Climbing;
 
 namespace Holdaballz
 {
@@ -45,39 +46,48 @@ namespace Holdaballz
             Main.CreateBall();
         }
 
+        public bool ThrowPhys;
         void Update()
         {
-            // Too lazy to figure out OnTrigger crap
+            if (Main.BallObj != null)
+            {
+                // Too lazy to figure out OnTrigger crap
 
-            if (ControllerInputPoller.instance.rightGrab && GorillaLocomotion.Player.Instance.rightControllerTransform.position.Distance(Main.BallObj.transform.position) < DistanceThingy && HandGrabbing != "left")  //Right Hand
-            {
-                Main.BallObj.transform.position = GorillaLocomotion.Player.Instance.rightControllerTransform.position;
-                Main.BallObj.transform.rotation = GorillaLocomotion.Player.Instance.rightControllerTransform.rotation;
-                Main.BallObj.GetComponent<Rigidbody>().useGravity = false;
-                DistanceThingy = 10f;
-                HandGrabbing = "right";
-            }
-            else if (ControllerInputPoller.instance.leftGrab && GorillaLocomotion.Player.Instance.leftControllerTransform.position.Distance(Main.BallObj.transform.position) < DistanceThingy && HandGrabbing != "right") //Left hand
-            {
-                Main.BallObj.transform.position = GorillaLocomotion.Player.Instance.leftControllerTransform.position;
-                Main.BallObj.transform.rotation = GorillaLocomotion.Player.Instance.leftControllerTransform.rotation;
-                Main.BallObj.GetComponent<Rigidbody>().useGravity = false;
-                DistanceThingy = 10f;
-                HandGrabbing = "left";
-            }
-            else //None
-            {
-                Main.BallObj.GetComponent<Rigidbody>().useGravity = true;
-                DistanceThingy = 0.1f;
-                HandGrabbing = "none";
-            }
+                if (ControllerInputPoller.instance.rightGrab && GorillaLocomotion.Player.Instance.rightControllerTransform.position.Distance(Main.BallObj.transform.position) < DistanceThingy && HandGrabbing != "left")  // Right Hand
+                {
+                    Main.BallObj.transform.position = GorillaLocomotion.Player.Instance.rightControllerTransform.position;
+                    Main.BallObj.transform.rotation = GorillaLocomotion.Player.Instance.rightControllerTransform.rotation;
+                    Main.BallObj.GetComponent<Rigidbody>().useGravity = false;
+                    ThrowPhys = true;
+                    DistanceThingy = 10f;
+                    HandGrabbing = "right";
+                }
+                else if (ControllerInputPoller.instance.leftGrab && GorillaLocomotion.Player.Instance.leftControllerTransform.position.Distance(Main.BallObj.transform.position) < DistanceThingy && HandGrabbing != "right") // Left hand
+                {
+                    Main.BallObj.transform.position = GorillaLocomotion.Player.Instance.leftControllerTransform.position;
+                    Main.BallObj.transform.rotation = GorillaLocomotion.Player.Instance.leftControllerTransform.rotation;
+                    Main.BallObj.GetComponent<Rigidbody>().useGravity = false;
+                    ThrowPhys = true;
+                    DistanceThingy = 10f;
+                    HandGrabbing = "left";
+                }
+                else //None
+                {
+                    if (ThrowPhys)
+                    {
+                        Main.BallObj.GetComponent<Rigidbody>().velocity = Main.BallObj.GetComponent<GorillaVelocityEstimator>().linearVelocity;
+                        ThrowPhys = false;
+                    }
+                    Main.BallObj.GetComponent<Rigidbody>().useGravity = true;
+                    DistanceThingy = 0.1f;
+                    HandGrabbing = "none";
+                }
 
-            // Out of map detector
-
-            if (Main.BallObj.transform.position.y < -90f)
-            {
-                Main.ResetBall();
-                Debug.Log("Ball reset");
+                if (GorillaLocomotion.Player.Instance.bodyCollider.transform.position.Distance(Main.BallObj.transform.position) < 10f) // Far from player detector
+                {
+                    Main.ResetBall();
+                    Debug.Log("Ball reset");
+                }
             }
         }
 
