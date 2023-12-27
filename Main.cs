2899ea7bcd050a54ca83869d8d40f47ec4cedbf7
ShinyGorilla <134111158+ShinyGorilla@users.noreply.github.com>
2023-12-27 04:04:24 -0700

    Fixed issues with enable and disable

--- Main.cs
+++ Main.cs
@@ -1,50 +1,124 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using HarmonyLib;
-using GorillaLocomotion;
 using BepInEx;
+using GorillaLocomotion;
+using GorillaNetworking;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Utilla;
-using System.IO;
-using System.Diagnostics;
-using BepInEx.Logging;
+using Holdaballz.Tools;
+using UnityEngine.Device;
+using ComputerPlusPlus;
+using Valve.VR.InteractionSystem;
+using Oculus.Platform;
+using UnityEngine.XR.Interaction.Toolkit.AffordanceSystem.Receiver.Primitives;
 using GorillaLocomotion.Climbing;
 
 namespace Holdaballz
 {
-    [HarmonyPatch(typeof(Player))]
-    [HarmonyPatch("LateUpdate", MethodType.Normal)]
-    public class Main
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
     {
-        public static GameObject BallObj;
-        public static void CreateBall()
+        bool Enabled;
+        bool InRoom;
+        string HandGrabbing;
+        float DistanceThingy = 0.1f;
+
+        void Start()
         {
-            //Too lazy to organize
-            BallObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            BallObj.GetComponent<Renderer>().material.color = new Color32(255, 255, 255, 255);
-            BallObj.GetComponent<Renderer>().material.shader = Shader.Find("GorillaTag/UberShader");
-            BallObj.AddComponent<Rigidbody>();
-            BallObj.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
-            BallObj.AddComponent<GorillaVelocityEstimator>();
-            BallObj.layer = 8;
-            BallObj.name = "Ball";
-            BallObj.transform.position = GorillaLocomotion.Player.Instance.rightControllerTransform.position;
-            BallObj.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            Utilla.Events.GameInitialized += OnGameInitialized;
         }
-        public static void DestroyBall()
+
+        void OnEnable()
         {
-            if (BallObj != null)
+            Enabled = true;
+
+            if (InRoom)
             {
-                GameObject.Destroy(BallObj);
-                BallObj = null;
+                Main.CreateBall();
+            }
+
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        void OnDisable()
+        {
+            Enabled = false;
+
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            
+        }
+
+        public bool ThrowPhys;
+        void Update()
+        {
+            if (Main.BallObj != null && Enabled && InRoom)
+            {
+                // Too lazy to figure out OnTrigger crap
+
+                if (ControllerInputPoller.instance.rightGrab && GorillaLocomotion.Player.Instance.rightControllerTransform.position.Distance(Main.BallObj.transform.position) < DistanceThingy && HandGrabbing != "left")  // Right Hand
+                {
+                    Main.BallObj.transform.position = GorillaLocomotion.Player.Instance.rightControllerTransform.position;
+                    Main.BallObj.transform.rotation = GorillaLocomotion.Player.Instance.rightControllerTransform.rotation;
+                    Main.BallObj.GetComponent<Rigidbody>().useGravity = false;
+                    ThrowPhys = true;
+                    DistanceThingy = 10f;
+                    HandGrabbing = "right";
+                }
+                else if (ControllerInputPoller.instance.leftGrab && GorillaLocomotion.Player.Instance.leftControllerTransform.position.Distance(Main.BallObj.transform.position) < DistanceThingy && HandGrabbing != "right") // Left hand
+                {
+                    Main.BallObj.transform.position = GorillaLocomotion.Player.Instance.leftControllerTransform.position;
+                    Main.BallObj.transform.rotation = GorillaLocomotion.Player.Instance.leftControllerTransform.rotation;
+                    Main.BallObj.GetComponent<Rigidbody>().useGravity = false;
+                    ThrowPhys = true;
+                    DistanceThingy = 10f;
+                    HandGrabbing = "left";
+                }
+                else //None
+                {
+                    if (ThrowPhys)
+                    {
+                        Main.BallObj.GetComponent<Rigidbody>().velocity = Main.BallObj.GetComponent<GorillaVelocityEstimator>().linearVelocity;
+                        ThrowPhys = false;
+                    }
+                    Main.BallObj.GetComponent<Rigidbody>().useGravity = true;
+                    DistanceThingy = 0.1f;
+                    HandGrabbing = "none";
+                }
+
+                if (GorillaLocomotion.Player.Instance.bodyCollider.transform.position.Distance(Main.BallObj.transform.position) > 10f) // Far from player detector
+                {
+                    Main.ResetBall();
+                }
+            }
+
+            if (Enabled == false)
+            {
+                Main.DestroyBall();
             }
         }
-        public static void ResetBall()
+
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
         {
-            DestroyBall();
-            CreateBall();
+            InRoom = true;
+
+            if (Enabled)
+            {
+                Main.CreateBall();
+            }
+        }
+
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            InRoom = false;
+            Main.DestroyBall();
         }
     }
 }
