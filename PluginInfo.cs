dd820de44e45d86ec58e0b5050f4a8a6ac89f7ff
ShinyGorilla <134111158+ShinyGorilla@users.noreply.github.com>
2023-12-31 06:10:08 -0700

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,6 +4,6 @@ namespace Holdaballz
     {
         public const string GUID = "ShinyGorilla.Holdaballz";
         public const string Name = "Holdaballz";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.4";
     }
 }
